06ca2213db5a1b0c7fd2bcaad88d29065100a01c
VitorEmanuelDSV <vitor.emanueldsvidal@gmail.com>
2024-09-27 01:03:19 -0300

    feat: implements metods

--- fachada/src/main/java/entities/academic/Disciplina.java
+++ fachada/src/main/java/entities/academic/Disciplina.java
@@ -10,4 +10,31 @@ public class Disciplina {
         this.nome = nome;
         this.horario = horario;
     }
+
+    // Getters and Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
 }
