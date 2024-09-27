2d60ecf91453563dda99b1b31c2d3039ea2da0a0
VitorEmanuelDSV <vitor.emanueldsvidal@gmail.com>
2024-09-26 22:55:32 -0300

    Update Aluno.java

--- fachada/src/main/java/entities/academic/Aluno.java
+++ fachada/src/main/java/entities/academic/Aluno.java
@@ -1,10 +1,5 @@
 package entities.academic;
 
-import relationships.academic.Historico;
-import relationships.academic.RDM;
-
 public class Aluno {
     private Dados dados;
-    private Historico historico;
-    private RDM rdm;
 }
