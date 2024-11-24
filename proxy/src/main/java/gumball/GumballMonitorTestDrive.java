1bce095164bf642646d171f21affb72bd8a03bb3
VitorEmanuelDSV <vitor.emanueldsvidal@gmail.com>
2024-11-24 15:53:23 -0300

    Update GumballMonitorTestDrive.java

--- proxy/src/main/java/gumball/GumballMonitorTestDrive.java
+++ proxy/src/main/java/gumball/GumballMonitorTestDrive.java
@@ -5,15 +5,17 @@ import java.rmi.*;
 public class GumballMonitorTestDrive {
  
 	public static void main(String[] args) {
-		String[] location = {"rmi://santafe.mightygumball.com/gumballmachine",
-		                     "rmi://boulder.mightygumball.com/gumballmachine",
-		                     "rmi://austin.mightygumball.com/gumballmachine"}; 
+		String[] location = {"rmi://santafe.mightygumball.com/gumballmachine"};
+
+		// String[] location = {"rmi://santafe.mightygumball.com/gumballmachine",
+		//                      "rmi://boulder.mightygumball.com/gumballmachine",
+		//                      "rmi://austin.mightygumball.com/gumballmachine"};
 		
-		if (args.length >= 0)
-        {
-            location = new String[1];
-            location[0] = "rmi://" + args[0] + "/gumballmachine";
-        }
+		// if (args.length >= 0)
+        // {
+        //     location = new String[1];
+        //     location[0] = "rmi://" + args[0] + "/gumballmachine";
+        // }
 		
 		GumballMonitor[] monitor = new GumballMonitor[location.length];
 		
